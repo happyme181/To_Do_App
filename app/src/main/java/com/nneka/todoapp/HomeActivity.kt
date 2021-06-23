c7367b9f5a76637feb63af5c02da85ae8115985d
happyme181 <nneka.eriavbe@gmail.com>
2021-06-23 17:49:41 +0100

    initial use

--- app/src/main/java/com/nneka/todoapp/HomeActivity.kt
+++ app/src/main/java/com/nneka/todoapp/HomeActivity.kt
@@ -1,12 +1,16 @@
 package com.nneka.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var usernameDisplay:TextView
+    private lateinit var profileButton: Button
+    private lateinit var newButton: Button
 
 
 
@@ -15,8 +19,29 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         usernameDisplay = findViewById(R.id.usernameDisplay)
+        profileButton = findViewById(R.id.profileButton)
+        newButton = findViewById(R.id.newButton)
+
 
         usernameDisplay.text = intent.getStringExtra("name")
 
+        profileButton.setOnClickListener{
+            openProfilePage()
+        }
+
+        newButton.setOnClickListener {
+            openNewPage()
+        }
+
     }
+    private fun openProfilePage(){
+        val myIntent = Intent (this, ProfileActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    private fun openNewPage(){
+        val intent = Intent (this, NewActivity::class.java)
+        startActivity(intent)
+    }
+
 }
